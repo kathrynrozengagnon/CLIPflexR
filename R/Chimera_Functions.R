663a45a0478581950d2e68cecf9d7fbb7248d3f3
kathrynrozengagnon <krozen@rockefeller.edu>
2020-08-30 23:20:19 -0400

    Update Chimera_Functions.R

--- R/Chimera_Functions.R
+++ R/Chimera_Functions.R
@@ -86,122 +86,95 @@ unbam <- function(bam,outfa=NULL){
 #'
 #' @author Kathryn Rozen-Gagnon
 #'
-#' @param BAMs BAM files to process.
+#' @param bams BAM files to process.
 #' @param knownMiRNAs knownMiRNAs FASTA file.
 #' @param genomeIndex Full genome index
+#' @param exclude contaminating sequences to remove 
 #' @return Path 
-#' @import GenomicAlignments
+#' @import GenomicAlignments BiocParallel purrr stringr
 #' @importMethodsFrom rtracklayer export.bed export.bw mcols
 #' @export
-chimera_Process <- function(bams,knownMiRNAs,genomeIndex,bpparam=NULL,verbose=TRUE){
-  if(!is.null(bpparam)) bpparam <- BiocParallel::SerialParam()
+chimera_Process <- function(bams,knownMiRNAs,genomeIndex,exclude, bpparam=NULL,verbose=TRUE){
+  if(is.null(bpparam)) bpparam <- BiocParallel::SerialParam()
   if(verbose) message("Extracting unmapped reads to FASTA..",appendLF = FALSE)
-  fastas <- bplappy(bams,unbam,bpparam=bpparam)
+  fastas <- bplapply(bams,unbam,BPPARAM=bpparam)
   if(verbose) message("done")
   if(verbose) message("Creating indicies from FASTA files..",appendLF = FALSE)
-  indicies <- bplappy(fastas,bowtie2_index,bpparam=bpparam)
+  indicies <- bplapply(fastas,bowtie2_index,BPPARAM=bpparam)
   if(verbose) message("done")
   if(verbose) message("Mapping miRNAs to unmapped reads..",appendLF = FALSE)
-  newBams <- bplappy(indicies,
+  newBams <- bplapply(indicies,
                       function(x,knownMiRNAs){
-                        bowtie_align(knownMiRNAs,index=x)
-                      },knownMiRNAs=knownMiRNAs,bpparam=bpparam)
+                        bowtie_align(knownMiRNAs,index=x, bam = paste0(x, ".bam"))
+                      },knownMiRNAs=knownMiRNAs,BPPARAM=bpparam)
   if(verbose) message("done")
   if(verbose) message("Converting BAMs to BEDs..",appendLF = FALSE)
-  beds <- bplapply(newBams, bamtobed,bpparam=bpparam)
+  beds <- bplapply(newBams, bamtobed,BPPARAM=bpparam)
   if(verbose) message("done")  
   
-  
-  # Not sure here...
-  chimera <- bplapply(beds, function(x) {
-    if (!file.size(x) == 0) {
-      read.delim(x, header = FALSE, sep = "")
-    }
-  },bpparam=bpparam)
-  names(chimera) <- beds
-  t <- lapply(chimera, nrow) 
-  t <- unlist(t) 
-  t <- names(t) #get names of beds with entried and read these in 
-  
-  
-  test <- lapply(t, read.delim, header = FALSE, sep = "")
-  names(test) <- t 
+  beds <- beds[file.size(unlist(beds))!=0]
+  chimera <- lapply(beds, read.delim, header = FALSE, sep = "")
+  names(chimera) <- beds 
   col.names <- c("rowname", "start", "stop", "name", "score", "strand")
-  test <- lapply(test, setNames, col.names)
-  t <- gsub(".bed", ".fa", t) #so t has names of all fastas to read in 
- 
-  fa <- fastas
+  chimera <- lapply(chimera, setNames, col.names)
+  fanams <- gsub(".bed", ".fa", beds)
   
-  fasta <- bplapply(fa, readDNAStringSet, format = "fasta", nrec = -1L,bpparam=bpparam)
+  fa <- fastas[fastas %in% fanams]
+  fasta <- bplapply(fa, readDNAStringSet, format = "fasta", nrec = -1L,BPPARAM=bpparam)
   names(fasta) <- fa 
-  fasta <- fasta[names(fasta) %in% t]
-  
-  # fasta <- lapply(fasta, as.data.frame)
-  fasta <- bplapplylapply(fasta, function(x) tibble::rownames_to_column(as.data.frame(x)),bpparam=bpparam)
-  
-  
+  fasta <- bplapply(fasta, function(x) tibble::rownames_to_column(as.data.frame(x)),BPPARAM=bpparam)
+  #check for duplicate readnames
+  if (verbose) message("Checking read names..", appendLF = FALSE)
+  checkreads <- lapply(fasta, function(x) duplicated(x$rowname))
+  checkreads <- unlist(lapply(checkreads, function(x) length(which(x==TRUE))))
+  idx <- which(checkreads > 0)
+  if (verbose & isTRUE(length(idx)==0)) message("read names ok") else message(paste0("Warning, the following sample(s) have duplicate read names! \n", names(idx)))
   #merge together read sequence and bed by rowname (read name)
-  chimera <- map2(fasta, test, ~merge(.x,.y, by = "rowname"))
-  # names(chimera) <- gsub("/rugpfs/fs0/rice_lab/scratch/krozen/AaegL5_mapped/unmapped_for_chimera/unmapped_", "/rugpfs/fs0/rice_lab/scratch/krozen/AaegL5_mapped/unmapped_for_chimera/known_novel_sRNA_revmapped/unmapped_", names(chimera))
-  names(chimera) <- names(chimera)
+  chimera <- map2(fasta,chimera, ~merge(.x,.y, by = "rowname"))
   
   #write files to chimera inputs: bed with read name, start, stop, srand, read name, and read sequence
   # setwd(Dir)
   fastaTxts <- vector("list",length = length(chimera))
-  for (x in names(chimera)) {
-    write.table(chimera[[x]], file=paste0(x,".txt"), sep="\t", quote = FALSE)
-    fastaTxts[[x]] <- paste0(x,".txt")
+  for (x in 1:length(chimera)) {
+    write.table(chimera[[x]], file=paste0(names(chimera)[x],".txt"), sep="\t", quote = FALSE)
+    fastaTxts[[x]] <- paste0(names(chimera)[x],".txt")
   }
- 
-  ####
-  # Eh?
-  ##
-  # write.table(test, file=".txt", sep="\t")
-  
-  
-  chimeraProcess <- function(input) {
-    
-    BR1 <- read.delim(input, header=T)
-    BR1 <- BR1[!grepl("AAAAAAAAAAAAAAAAAAAAAAAAA", BR1$name),] #this was high frequency in my libraries removed
-    BR1<- BR1[BR1$strand=="+",]
-    BR1 <- BR1[duplicated(BR1$rowname)==F,] #come back to this, to remove reads with more than one miRNA mapped 
-    BR1$length <- sapply(as.character(BR1$x), nchar)
-    BR1$ups.seq <- mapply(substr, x=BR1$x, start=0, stop=BR1$start)
-    BR1$dns.seq <- mapply(substr, x=BR1$x, start=BR1$stop, stop=BR1$length)
-    outname = paste(input, sep = "")
-    outname = gsub(".fa.txt", "_chimera.txt", outname)
-    write.table(BR1, outname, quote=F, sep="\t")
-    return(outname)
-  }
-  files <- vector("list",length = length(fastaTxts))
+ names(fastaTxts) <- names(chimera)
+
+  chimerafiles <- vector("list",length = length(fastaTxts))
   for (i in 1:length(fastaTxts)) {
-    files[[i]] <- chimeraProcess(fastaTxts[i])
-  }
-  rffiles <- vector("list",length = length(files))  
-  for (i in 1:length(files)) {
-    rffiles[[i]] <- reformat(files[i])
+    chimerafiles[[i]] <- chimeraProcess(fastaTxts[[i]], exclude)
   }
   
+  rffiles <- vector("list",length = length(chimerafiles)) 
+
+  for (i in 1:length(chimerafiles)) {
+    rffiles[[i]] <- reformat(chimerafiles[[i]])
+  }
   
   
-  remappedBams <- bplapply(rffiles, bowtie_align, index =genomeIndex,bpparam=bpparam)
   
-  myBeds <- bplapply(remappedBams, bamtobed,bpparam=bpparam)
+  remappedBams <- bplapply(rffiles, bowtie_align, index =genomeIndex,BPPARAM=bpparam)
+  
+  myBeds <- bplapply(remappedBams, bamtobed,BPPARAM=bpparam)
   
   
   return(myBeds)
   
 }
 
-chimeraProcess <- function(input) {
-  
+chimeraProcess <- function(input, exclude) {
   BR1 <- read.delim(input, header=T)
-  BR1 <- BR1[!grepl("AAAAAAAAAAAAAAAAAAAAAAAAA", BR1$name),] #this was high frequency in my libraries removed
+  BR1 <- BR1[!BR1$name %in% exclude,] #this was high frequency in my libraries removed
   BR1<- BR1[BR1$strand=="+",]
-  BR1 <- BR1[duplicated(BR1$rowname)==F,] #come back to this, to remove reads with more than one miRNA mapped 
+    BR1$miRNAnum <- ifelse(!grepl("miR|let|iab", BR1$name), str_sub(BR1$name, 2, 7), NA)
+    BR1$miRNAnum <- ifelse(grepl("miR|let|iab", BR1$name), as.numeric(apply(BR1,1,function(x) regmatches(x["name"],regexpr("[0-9]+",x["name"])))), paste(BR1$miRNAnum))
+    BR1 <- BR1[order(BR1$rowname, BR1$start, BR1$miRNAnum, BR1$name),]
+    BR1 <- BR1[!duplicated(BR1$rowname),] 
+    BR1$miRNAnum <- NULL
   BR1$length <- sapply(as.character(BR1$x), nchar)
   BR1$ups.seq <- mapply(substr, x=BR1$x, start=0, stop=BR1$start)
-  BR1$dns.seq <- mapply(substr, x=BR1$x, start=BR1$stop, stop=BR1$length)
+  BR1$dns.seq <- mapply(substr, x=BR1$x, start=BR1$stop+1, stop=BR1$length)
   outname = paste(input, sep = "")
   outname = gsub(".fa.txt", "_chimera.txt", outname)
   write.table(BR1, outname, quote=F, sep="\t")
@@ -210,17 +183,13 @@ chimeraProcess <- function(input) {
 
 reformat <- function(input) {
   BR1 <- read.delim(input, header=T)
-  BR1$V8 <- paste(BR1$rowname, BR1$name, sep = ";")
-  BR1 <- BR1[c(11,10 )]
+  BR1$ID <- paste(BR1$rowname, BR1$name, sep = ";") 
+  BR1 <- BR1[c("ID","dns.seq")]
   BR1$dns.seq <- as.character(BR1$dns.seq)
-  BR1$length <- nchar(BR1$dns.seq)
-  BR1 <- BR1[BR1$length >= 18,]
-  BR1 <- BR1[1:2]
+  BR1 <- BR1[nchar(BR1$dns.seq)>=18,] 
   outname = paste(input, '.fa', sep = "")
   BR2 <- DNAStringSet(BR1$dns.seq, use.names = TRUE)
-  names(BR2) <- BR1$V8
-  writeXStringSet(BR2, outname, format ="fasta") 
+  names(BR2) <- BR1$ID
+  writeXStringSet(BR2, outname, format ="fasta")  
   return(outname)
 }
-
-
